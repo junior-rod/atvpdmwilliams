2cd2a9f3da3b336acbe9960d376378e6085bec15
junior-rod <williamsjrcontato@gmail.com>
2024-04-09 11:12:16 -0300

    Correção de erro de clonagem

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -7,28 +7,51 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.EditText
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("PDM24.1","No onCreate, $percentual")
+        if (savedInstanceState != null) {
+            percentual=savedInstanceState.getDouble("percentual")
+        }
+        Log.d("PDM24", "No onCreate, $percentual")
 
-        percentual=0.7
-        val btCalc: Button = findViewById(R.id.btCalcular)
-        val textMsg:TextView= findViewById(R.id.textMsg)
-        //btCalc.setBackgroundColor(Color.CYAN)
-        btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
-            Log.d("PDM24","No btCalcular, $percentual")
-        })
+        val gas:EditText = findViewById(R.id.gas)
+        val alcool:EditText = findViewById(R.id.alcool)
+        val swPercentual:Switch = findViewById(R.id.swPercentual)
+        val btCalcular:Button = findViewById(R.id.btCalcular)
+        val resultado:TextView= findViewById(R.id.textMsg)
+
+        btCalcular.setOnClickListener {
+            calcularMelhorPreco(gas.text.toString(), alcool.text.toString(), swPercentual.isChecked, resultado)
+        }
+
     }
+    fun calcularMelhorPreco(gasolina: String, alcool: String, percentual75: Boolean, resultado: TextView) {
+        val precoGasolina = gasolina.toDoubleOrNull()
+        val precoAlcool = alcool.toDoubleOrNull()
+
+        if (precoGasolina != null && precoAlcool != null) {
+            val percentual = if (percentual75) 0.75 else 0.7
+            val resultadoCalculo = precoAlcool / precoGasolina
+
+            resultado.text = if (resultadoCalculo >= percentual) {
+                "Melhor utilizar Gasolina"
+            } else {
+                "Melhor utilizar Álcool"
+            }
+        } else {
+            resultado.text = "Por favor, insira valores válidos."
+        }
+    }
+
 override fun onResume(){
     super.onResume()
-    Log.d("PDM24","No onResume, $percentual")
+    Log.d("PDM24","No onResume")
 }
 override fun onStart(){
     super.onStart()
@@ -46,4 +69,9 @@ override fun onDestroy(){
     super.onDestroy()
     Log.wtf("PDM24","No Destroy")
 }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putDouble("percentual",percentual)
+        super.onSaveInstanceState(outState)
+    }
+
 }
